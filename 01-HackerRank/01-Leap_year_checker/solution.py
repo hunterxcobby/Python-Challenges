0518a93efd36487efa1819c9934cca44039c5b4f
hunterxcobby <solomonsefah13@gmail.com>
2023-11-17 21:23:47 +0000

    add slotution

---
+++ 01-HackerRank/01-Leap_year_checker/solution.py
@@ -0,0 +1,9 @@
+def is_leap(year):
+    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+        return True
+    else:
+        return False
+
+
+year = int(input())
+print(is_leap(year))
