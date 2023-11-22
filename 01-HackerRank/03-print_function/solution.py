aafb5ea6dec8985a183d3970f09effaffb28da88
hunterxcobby <solomonsefah13@gmail.com>
2023-11-22 23:12:41 +0000

    solution

---
+++ 01-HackerRank/03-print_function/solution.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+if __name__ == '__main__':
+    n = int(input())
+
+    # Using a loop to iterate from 1 to n
+    for i in range(1, n + 1):
+        print(i, end='')
+
+    print()
