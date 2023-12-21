9f72675bf105cdf57a05252b9f3fba437e248d00
Cobby Sefah Solomon <solomonsefah13@gmail.com>
2023-12-21 21:54:53 +0000

    Update solution.py

---
+++ 01-HackerRank/09-sWAP_Case/solution.py
@@ -0,0 +1,7 @@
+def swap_case(s: str) -> str:
+    return s.swapcase()
+
+# Test the function with the provided sample
+sample_input = "HackerRank.com presents \"Pythonist 2\"."
+result = swap_case(sample_input)
+print(result)
