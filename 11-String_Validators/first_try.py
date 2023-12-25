850ada712f1a1c2a06d714939522c3f7d4560528
Cobby Sefah Solomon <solomonsefah13@gmail.com>
2023-12-25 20:49:32 +0000

    Create first_try.py

---
+++ 11-String_Validators/first_try.py
@@ -0,0 +1,23 @@
+if __name__ == '__main__':
+    s = input()
+    
+if s.isalnum():
+    print("True")
+else:
+    print("False")
+
+if s.isdigit():
+    print("True")
+else:
+    print("False")
+  
+if s.islower():
+    print("True")
+else:
+    print("False")
+    
+if s.isalpha():
+    print("True")
+else:
+    print("False")
+    
