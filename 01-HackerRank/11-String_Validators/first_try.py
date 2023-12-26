8002ac87ac093edc83cfb3d2df25f303de3b3050
hunterxcobby <solomonsefah13@gmail.com>
2023-12-26 21:53:18 +0000

    add string validators

---
+++ 01-HackerRank/11-String_Validators/first_try.py
@@ -0,0 +1,22 @@
+if __name__ == '__main__':
+    s = input()
+
+    if s.isalnum():
+        print("True")
+    else:
+        print("False")
+
+    if s.isdigit():
+        print("True")
+    else:
+        print("False")
+
+    if s.islower():
+        print("True")
+    else:
+        print("False")
+
+    if s.isalpha():
+        print("True")
+    else:
+        print("False")
