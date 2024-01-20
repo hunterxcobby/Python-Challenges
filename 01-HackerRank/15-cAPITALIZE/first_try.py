7b9c580282541862e9746b4c77672b22240eb82c
hunterxcobby <solomonsefah13@gmail.com>
2024-01-20 22:24:42 +0000

    first attempt

---
+++ 01-HackerRank/15-cAPITALIZE/first_try.py
@@ -0,0 +1,15 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the solve function below.
+def solve(s):
+    print(s.capitalize())
+
+
+if __name__ == '__main__':
+    solve("string")
